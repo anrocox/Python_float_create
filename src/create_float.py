ca3139accc98e6c145933269cc12be48c32ac1fa
Andres Rodriguez <anrocox@gmail.com>
2014-07-17 14:07:31 -0500

    How to define a number of type float in Python?

---
+++ src/create_float.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 16, 2014
+
+@author: anroco
+
+How to define a number of type float in Python?
+
+¿Cómo definir un numero de tipo float en Python?
+'''
+
+#define the entire and decimal part separated by a dot
+f = 57.32
+print(f)
+
+#define the entire part continued of a dot
+f = 57.
+print(f)
+
+#definition in exponential notation
+f = 5.732e5
+print(f)
+
+#the float() method generate a float number from a string or number.
+f = float(57.32)
+print(f)
+
+import math
+
+#as a result of a function or method of an object.
+f = math.exp(1)
+print(f)
